2f6d654cd34143de1d465f095d433142bf31bf17
Tommy Hu <tomxhu@gmail.com>
2016-02-27 14:53:17 -0500

    hooked up stats endpoints

--- directives/dashboard.js
+++ directives/dashboard.js
@@ -1,7 +1,18 @@
+var _ = require('lodash');
+
 module.exports = function(myApp) {
     myApp.directive('dashboard', function() {
         return {
-            templateUrl: 'dashboard.html'
+            templateUrl: 'dashboard.html',
+            controller: function($scope, apiUrl, $http) {
+            	var stats = ['agent', 'application', 'homeOffice', 'job', 'order', 'work', 'education', 'document', 'user', 'lookup', 'template', 'skill'];
+            	_.map(stats, function(stat) {
+	                $http.get(apiUrl + stat + "/count", {headers: {'x-access-token': localStorage.getItem('semper-admin-token') }}).then(function (response) {
+						$scope[stat] = response.data.count;
+					});
+            	})
+            	console.log($scope)
+            }
         };
     });
 };
