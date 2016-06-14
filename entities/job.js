6ad720fa7481803bf1a8b44a1d4d97576e2510e2
Stephan Smith <stephan.smith.bc93@gmail.com>
2016-06-14 15:07:04 -0400

    Added in job homeoffice search options

--- entities/job.js
+++ entities/job.js
@@ -3,6 +3,24 @@ module.exports = function (nga) {
     var job = nga.entity('job').identifier(nga.field('_id'));
     job.label('Jobs');
 
+    var homeoOffices = [
+        { value: "atlanta", label: "Atlanta", id: '107' },
+	    { value: "baltimore", label: "Baltimore", id: '109' },
+	    { value: "boston", label: "Boston", id: '100' },
+	    { value: "chicago", label: "Chicago", id: '103' },
+	    { value: "dallas", label: "Dallas", id: '106' },
+	    { value: "international", label: "International", id: '0' },
+	    { value: "losAngeles", label: "Los Angeles", id: '108' },
+	    { value: "minneapolisStPaul", label: "Minneapolis-St Paul", id: '120' },
+	    { value: "newJersey", label: "New Jersey", id: '118' },
+	    { value: "newYorkCity", label: "New York", id: '122' },
+	    { value: "orangeCounty", label: "Orange County", id: '117'},
+	    { value: "otherLocations", label: "Other Locations", id: '0' },
+	    { value: "sanDiego", label: "San Diego", id: '121' },
+	    { value: "sanFrancisco", label: "San Francisco", id: '114' },
+	    { value: "santaClara", label: "Santa Clara", id: '115' }
+    ];
+
     job.listView()
     .title('Jobs')
     .perPage(50)
@@ -26,10 +44,26 @@ module.exports = function (nga) {
             .label('')
             .pinned(true)
             .template('<div class="input-group"><input type="text" ng-model="value" placeholder="Search" class="form-control"></input><span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span></div>'),
-        nga.field('type').label('Type'),
         nga.field('username', 'string').label('Username'),
-        nga.field('email', 'string').label('email'),
-        nga.field('city', 'string').label('City'),
+        nga.field('id_office', 'choice')
+            .label('Home Office')
+            .choices([
+                { label: "Atlanta", value: '107' },
+                { label: "Baltimore", value: '109' },
+                { label: "Boston", value: '100' },
+                { label: "Chicago", value: '103' },
+                { label: "Dallas", value: '106' },
+                { label: "International", value: '0' },
+                { label: "Los Angeles", value: '108' },
+                { label: "Minneapolis-St Paul", value: '120' },
+                { label: "New Jersey", value: '118' },
+                { label: "New York", value: '122' },
+                { label: "Orange County", value: '117'},
+                { label: "Other Locations", value: '0' },
+                { label: "San Diego", value: '121' },
+                { label: "San Francisco", value: '114' },
+                { label: "Santa Clara", value: '115' }
+            ]),
         nga.field('state', 'string').label('State')
     ]);
 
