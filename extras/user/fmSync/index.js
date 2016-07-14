553bbbedb346f279d4d7f27d1d6d7da1f2108b42
Stephan Smith <stephan.smith.bc93@gmail.com>
2016-07-14 14:52:44 -0400

    Added in FM fixes for the linker

--- extras/user/fmSync/index.js
+++ extras/user/fmSync/index.js
@@ -22,7 +22,25 @@ module.exports = function (myApp) {
                     { title: 'Address Line 2', webfld: 'address_2', fmfld: 'address_2' },
                     { title: 'City', webfld: 'address_city', fmfld: 'address_city' },
                     { title: 'State', webfld: 'address_state', fmfld: 'address_state' },
-                    { title: 'Zip', webfld: 'address_zip', fmfld: 'address_zip' }
+                    { title: 'Zip', webfld: 'address_zip', fmfld: 'address_zip' },
+                    { title: 'travel_time', webfld: 'travel_time', fmfld: 'travel_time' },
+                    { title: 'staff_recruiter', webfld: 'staff_recruiter', fmfld: 'staff_recruiter' },
+                    { title: 'flag_no_email', webfld: 'flag_no_email', fmfld: 'flag_no_email' },
+                    { title: 'transportation', webfld: 'transportation', fmfld: 'transportation' },
+                    { title: 'region_work', webfld: 'region_work', fmfld: 'region_work' },
+                    { title: 'shifts', webfld: 'shifts', fmfld: 'shifts' },
+                    { title: 'app_working_company', webfld: 'app_working_company', fmfld: 'app_working_company' },
+                    { title: 'Reason for Leaving Last', webfld: 'app_reason_leave_last', fmfld: 'app_reason_leave_last' },
+                    { title: 'flag_union', webfld: 'flag_union', fmfld: 'flag_union' },
+                    { title: 'source', webfld: 'source', fmfld: 'source' },
+                    { title: 'app_career_accomp', webfld: 'app_career_accomp', fmfld: 'app_career_accomp' },
+                    { title: 'app_company_prefer_not_contact', webfld: 'app_company_prefer_not_contact', fmfld: 'app_company_prefer_not_contact' },
+                    { title: 'app_industry', webfld: 'app_industry', fmfld: 'app_industry' },
+                    { title: 'pay_rate_desired', webfld: 'pay_rate_desired', fmfld: 'pay_rate_desired' },
+                    { title: 'app_why_semper', webfld: 'app_why_semper', fmfld: 'app_why_semper' },
+                    { title: 'app_what_heard_semper', webfld: 'app_what_heard_semper', fmfld: 'app_what_heard_semper' },
+                    { title: 'app_be_in_five_years', webfld: 'app_be_in_five_years', fmfld: 'app_be_in_five_years' },
+                    { title: 'flag_relocate_willing', webfld: 'flag_relocate_willing', fmfld: 'flag_relocate_willing' }
                 ];
 
                 $scope.saveToWeb = false;
@@ -37,7 +55,28 @@ module.exports = function (myApp) {
                 };
 
                 $scope.saveDataToFM = function() {
-                    alert('Coming soon');
+                    if (confirm('Are you sure you want to save data to the filemaker?')) {
+                        var data = {};
+                        _.each($scope.fields, function(fld) {
+                            /* Only save to FM when we have data. */
+                            if ($scope.userData[fld.webfld]) {
+                                data[fld.fmfld] = $scope.userData[fld.webfld];
+                            }
+                        });
+
+                        delete data.region_work;
+                        delete data.shifts;
+
+                        /* Add in the keys needs to save. */
+                        data.id_employee = $scope.user.id_employee;
+                        data.webId = $scope.user._id;
+
+                        $scope.userData.customPUT({ data: data }, 'filemaker').then(function(resp) {
+                            $scope.fmData = resp.result;
+                        }, function(err) {
+                            $scope.errorMessage = 'Unable to save data from Filemaker';
+                        });
+                    }
                 };
 
                 $scope.useAllFM = function() {
@@ -66,7 +105,18 @@ module.exports = function (myApp) {
                             }
                         });
 
-                        console.log('new webData', data);
+                        delete data.region_work;
+                        delete data.shifts;
+
+                        /* Add in the keys needs to save. */
+                        data.id_employee = $scope.user.id_employee;
+                        data.webId = $scope.user._id;
+
+                        $scope.userData.customPUT({ data: data }, 'filemaker').then(function(resp) {
+                            $scope.fmData = resp.result;
+                        }, function(err) {
+                            $scope.errorMessage = 'Unable to get data from Filemaker';
+                        });
                     }
                 };
 
