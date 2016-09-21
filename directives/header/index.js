7bbd1b00b79b6ff8ce60f6e168e023665fe2e167
Stephan Smith <stephan.smith.bc93@gmail.com>
2016-09-21 15:46:30 -0400

    Added in fixes to make the UI easier to customize

--- directives/header/index.js
+++ directives/header/index.js
@@ -2,7 +2,9 @@ module.exports = function(myApp) {
     myApp.directive('header', function() {
         return {
             template: require('./header.html'),
-            controller: function($scope, Restangular) {
+            controller: function($scope, Restangular, apiUrl) {
+
+                $scope.apiDomain = apiUrl.replace('https://', '').replace('/', '');
 
                 Restangular.all('user').customGET('current').then(function(data) {
                     $scope.user = data.data ? data.data : data;
@@ -11,7 +13,7 @@ module.exports = function(myApp) {
                 $scope.logout = function() {
                     if (confirm('Do you want to logout?')) {
                         localStorage.removeItem('semper-admin-token');
-                        window.location = './index.html';
+                        window.location = '/';
                     }
                 };
             }
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var enforce = require('express-sslify');
+var exphbs  = require('express-handlebars');
+var path = require('path');
 
 /* Setup the express app. */
 var app = express();
@@ -10,6 +12,24 @@ app.use(express.static('./'));
 if (process.env.NODE_ENV == 'production') {
 	app.use(enforce.HTTPS({ trustProtoHeader: true }));
 }
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+// This allows us to controll the apiUrl from the heroku config and
+// support different testing branches.
+
+app.locals.apiUrl = process.env.apiUrl || 'https://api.semperllc.com/';
+
+app.get('/login', function (req, res) {
+	res.render('login');
+});
+
+app.get('/', function (req, res) {
+	res.render('home');
+});
 
 app.listen(port);
 
